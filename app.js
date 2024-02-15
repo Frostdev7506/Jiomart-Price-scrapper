c47449c2034b8756af956da7215c6927a5d28ea5
Neeraj Butola <Neerajbutola234@gmail.com>
2024-02-16 03:13:03 +0530

    added fruits section as well

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 
 const {
   fetchJiomartVegetablesDataController,
+  fetchJiomartFruitsDataController,
 } = require("./controller/scrapeController");
 // Parse JSON request body
 app.use(express.json());
@@ -12,4 +13,5 @@ app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.get("/api/jiomartfreshfruits", fetchJiomartFruitsDataController);
 app.get("/api/jiomartfreshvegies", fetchJiomartVegetablesDataController);
--- controller/scrapeController.js
+++ controller/scrapeController.js
@@ -4,23 +4,29 @@ const express = require("express");
 exports.fetchJiomartVegetablesDataController = async (req, res) => {
   const url =
     "https://www.jiomart.com/c/groceries/fruits-vegetables/fresh-vegetables/229";
-  const data = await scrapeProduct(url);
+  const PriceElements = ".plp-card-details-price span.jm-heading-xxs.jm-mb-xxs";
+  const NameElements =
+    ".plp-card-details-name.line-clamp.jm-body-xs.jm-fc-primary-grey-80";
+  const data = await scrapeProduct(url, PriceElements, NameElements);
   res.json(data);
 };
-async function scrapeProduct(url) {
+exports.fetchJiomartFruitsDataController = async (req, res) => {
+  const url =
+    "https://www.jiomart.com/c/groceries/fruits-vegetables/fresh-fruits/220";
+  const PriceElements = ".plp-card-details-price span.jm-body-s-bold.jm-mr-xxs";
+  const NameElements =
+    ".plp-card-details-name.line-clamp.jm-body-xs.jm-fc-primary-grey-80";
+  const data = await scrapeProduct(url, PriceElements, NameElements);
+  res.json(data);
+};
+
+async function scrapeProduct(url, PriceElements, NameElements) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
-  const priceElement = await page.$(
-    ".plp-card-details-price span.jm-heading-xxs.jm-mb-xxs"
-  );
-  const price = await page.evaluate((el) => el.textContent, priceElement);
-
   // Select all elements that match the given selector
-  const priceElements = await page.$$(
-    ".plp-card-details-price span.jm-heading-xxs.jm-mb-xxs"
-  );
+  const priceElements = await page.$$(PriceElements);
 
   // Iterate over each element to extract its text content
   const prices = await Promise.all(
@@ -29,15 +35,8 @@ async function scrapeProduct(url) {
     })
   );
 
-  const nameElement = await page.$(
-    ".plp-card-details-name.line-clamp.jm-body-xs.jm-fc-primary-grey-80"
-  );
-  const name = await page.evaluate((el) => el.textContent, nameElement);
-
   // Select all elements that match the given selector
-  const nameElements = await page.$$(
-    ".plp-card-details-name.line-clamp.jm-body-xs.jm-fc-primary-grey-80"
-  );
+  const nameElements = await page.$$(NameElements);
 
   // Iterate over each element to extract its text content
   const names = await Promise.all(
