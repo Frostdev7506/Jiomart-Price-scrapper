4dcc1e0c1317d289238aed5dc8e7bedd22e39076
Neeraj Butola <Neerajbutola234@gmail.com>
2024-02-17 02:19:14 +0530

    added root get route

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.listen(PORT || 3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+app.get("/", (req, res) => {
+  res.send(
+    "The Scrapper server is up and running!Currenly wokring routes are : \n /api/jiomartfreshfruits \n /api/jiomartfreshvegies \n /api/jiomartatta"
+  );
+});
+
 app.get("/api/jiomartfreshfruits", fetchJiomartFruitsDataController);
 app.get("/api/jiomartfreshvegies", fetchJiomartVegetablesDataController);
 app.get("/api/jiomartatta", fetchJiomartAttaDataController);
